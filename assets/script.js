50556a1e6862a9366708c5194aad67777b3eefb1
Jose Lemuz <lemuz.jose@gmail.com>
2022-02-12 01:54:04 -0800

    fetch data with city name and render to DOM

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,49 @@
 var getWeather = function(){
-    var weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=60.99&lon=30.9&exclude=minutely,hourly&appid=8a15f2e8988dd05df66461025f4b2471&units=imperial';
+ var cityEl= document.querySelector('#city').value;
+ console.log(cityEl);
+    var weatherURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityEl}&limit=5&appid=8a15f2e8988dd05df66461025f4b2471`;
 
     fetch(weatherURL)
     .then(response => response.json())
     .then(data => {
-        console.log(data.daily);
-        console.log(data.daily[0].temp.day);
-        console.log(data.daily[0].humidity);
-        console.log(data.daily[0].uvi);
+        const lat = data[0].lat;
+        const lon = data[0].lon;
 
-        console.log(data.daily[0].weather[0].main)
+        console.log(lat+' '+lon);
 
+        return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=8a15f2e8988dd05df66461025f4b2471&units=imperial`)
+        .then(response => response.json())
+        .then(
+            data=>{
+                console.log(data);
+                getDailyWeather(data);
+            }
+        )
 
+        // console.log(data.daily[0].weather[0].main)
+        //console.log(data[0].lat);
+        //console.log(data[0].lon);
+
+     
+        //getDailyWeather(data);
     }
     
     );
 }    
 
+
+function getDailyWeather(data){
+      clearStart();
+    for(i=0; i<6; i++){
+        console.log('Temp ' + i + ': ' + data.daily[i].temp.day + ' Humidity: ' + data.daily[i].humidity + ' UV Index: '+ data.daily[i].uvi
+        + ' Wind Speed '+ data.daily[i].wind_speed);
+        var bod = document.querySelector('#data');
+        var weather = document.createElement('li');
+        weather.textContent=('Temp ' + i + ': ' + data.daily[i].temp.day + ' Humidity: ' + data.daily[i].humidity + ' UV Index: '+ data.daily[i].uvi
+        + ' Wind Speed '+ data.daily[i].wind_speed);
+        bod.appendChild(weather);
+    }
+}
 
 //City, Date, Icon
 //Temp
@@ -31,6 +58,13 @@ var getWeather = function(){
 //Temp
 //Wind
 //Humidity
+function clearStart(){
+    clear = document.querySelector('#data');
+    while (clear.firstChild) {
+        clear.removeChild(clear.firstChild);
+    }
+}
+    
 
     
 document.querySelector('#test').addEventListener('click', getWeather);
