90d0c4665a1b2627940304f5aa36b34613601ba9
Jose Lemuz <lemuz.jose@gmail.com>
2022-02-20 16:44:20 -0800

    implemented icons

--- assets/script.js
+++ assets/script.js
@@ -19,52 +19,105 @@ var getWeather = function(){
                 getDailyWeather(data);
             }
         )
-
-        // console.log(data.daily[0].weather[0].main)
-        //console.log(data[0].lat);
-        //console.log(data[0].lon);
-
-     
-        //getDailyWeather(data);
     }
     
     );
 }    
 
+var date = new Date();
+var today = new Date();
 
 function getDailyWeather(data){
-      clearStart();
-    for(i=0; i<6; i++){
+    clearStart();
+    var cityEl= document.querySelector('#city').value;
+
+    var card = document.querySelector(`#grid-card-${0}`);
+
+    var temp = document.createElement('div');
+    temp.setAttribute('class', 'temp');
+    temp.textContent = ('Temp: ' + data.daily[0].temp.day);
+
+
+    var day = document.createElement('div');
+    day.setAttribute('id', 'current-city');
+    var date = moment().add(i, 'days');
+    var today = moment().format('MM/DD/YYYY');
+    day.innerHTML = cityEl + ` (${today}) ` + `<img src="http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@4x.png"  width = "30px" length="30px"></img>`;
+
+    var wind = document.createElement('div');
+    wind.setAttribute('class', 'wind');
+    wind.textContent = ('Wind: ' + data.daily[0].wind_speed + ' MPH');
+
+    var humidity = document.createElement('div');
+    humidity.setAttribute('class', 'humidity');
+    humidity.textContent = ('Humidity: ' + data.daily[i].humidity +  '%');
+
+    var uvi =document.createElement('div');
+    var uvi_deci =  data.daily[0].uvi/10;
+    if(uvi_deci<.30){
+        uvi.setAttribute('class', 'favorable');
+    }
+    else if (uvi_deci<.75){
+        uvi.setAttribute('class', 'moderate');
+    }
+    else{
+        uvi._deci.setAttribute('class', 'severe')
+    };
+    uvi.textContent=('UV Index: ' + uvi_deci.toFixed(2));
+
+ 
+    card.appendChild(day);
+    card.appendChild(temp);
+    card.appendChild(wind);
+    card.appendChild(humidity);
+    card.appendChild(uvi);    
+    
+    for(i=1; i<6; i++){
+        
         console.log('Temp ' + i + ': ' + data.daily[i].temp.day + ' Humidity: ' + data.daily[i].humidity + ' UV Index: '+ data.daily[i].uvi
         + ' Wind Speed '+ data.daily[i].wind_speed);
-        var bod = document.querySelector('#data');
-        var weather = document.createElement('li');
-        weather.textContent=('Temp ' + i + ': ' + data.daily[i].temp.day + ' Humidity: ' + data.daily[i].humidity + ' UV Index: '+ data.daily[i].uvi
-        + ' Wind Speed '+ data.daily[i].wind_speed);
-        bod.appendChild(weather);
+
+        var card = document.querySelector(`#grid-card-${i}`);
+
+        var day = document.createElement('div');
+        var date = moment().add(i, 'days');
+        day.textContent= date.format('MM/DD/YYYY');
+        day.setAttribute('class', 'day');
+
+        var icon = document.createElement('div');
+        icon.setAttribute('class', 'icon');
+        icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@4x.png"  width = "50px" length="50px"></img>`;
+
+        var temp = document.createElement('div');
+        temp.setAttribute('class', 'temp');
+        temp.textContent = ('Temp: ' + data.daily[i].temp.day);
+
+        var wind = document.createElement('div');
+        wind.setAttribute('class', 'wind');
+        wind.textContent = ('Wind: ' + data.daily[i].wind_speed + ' MPH');
+
+        var humidity = document.createElement('div');
+        humidity.setAttribute('class', 'humidity');
+        humidity.textContent = ('Humidity: ' + data.daily[i].humidity + ' %');
+
+        card.appendChild(day);
+        card.appendChild(icon);
+        card.appendChild(temp);
+        card.appendChild(wind);
+        card.appendChild(humidity);
+
     }
 }
 
-//City, Date, Icon
-//Temp
-//Wind
-//Humidity
-//UV Index
 
-
-//5 Day Forecast
-//Date
-//Icon
-//Temp
-//Wind
-//Humidity
 function clearStart(){
-    clear = document.querySelector('#data');
+    for(i=0;i<6;i++){
+    clear = document.querySelector(`#grid-card-${i}`);
     while (clear.firstChild) {
         clear.removeChild(clear.firstChild);
     }
 }
-    
+}
 
     
-document.querySelector('#test').addEventListener('click', getWeather);
+document.querySelector('#search').addEventListener('click', getWeather);
